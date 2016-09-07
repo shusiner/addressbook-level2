c3185458cce07941b83a7073989ea011d51cacf4
ng huan ran <quantum.plato@gmail.com>
2016-09-07 15:13:16 +0800

    T4A2 Use class-level members

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -14,6 +14,8 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 0;
 
     private final UniqueTagList tags;
     /**
@@ -25,6 +27,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        Person.nextSequenceNumber++;
+        this.sequenceNumber = nextSequenceNumber;
     }
 
     /**
@@ -53,13 +57,17 @@ public class Person implements ReadOnlyPerson {
     public Address getAddress() {
         return address;
     }
-
+    
+    public int getSequenceNumber() {
+		return sequenceNumber;
+	}
+    
     @Override
     public UniqueTagList getTags() {
         return new UniqueTagList(tags);
     }
 
-    /**
+	/**
      * Replaces this person's tags with the tags in the argument tag list.
      */
     public void setTags(UniqueTagList replacement) {
